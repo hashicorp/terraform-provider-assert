ac0d12b30645d5f9a1bdcb216086c2ea75cc2d7b
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-04-04 15:28:26 +0200

    Update tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -15,4 +15,4 @@ import (
 // to ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive
 // Generate documentation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir ..
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate
