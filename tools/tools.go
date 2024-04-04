797e08fa33b4df5ab6c9139f35bec740a2824b2e
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-04-04 15:24:16 +0200

    Update tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -15,6 +15,6 @@ import (
 // Format Terraform code for use in documentation.
 // If you do not have Terraform installed, you can remove the formatting command, but it is suggested
 // to ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ../examples/
+//go:generate terraform fmt -recursive
 // Generate documentation.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir ..
